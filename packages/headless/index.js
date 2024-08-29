bcdaeed185a8f703620c72af0e73f5c3f8626c78
robinma <ahmzj@163.com>
2024-08-30 00:13:39 +0800

    dev

---
+++ packages/headless/index.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+
+const extensionPath = '/Users/majie/mw/project/extension_webrtc/packages/tabstream';
+const extensionId = 'akniocjldhfgnjjlicnkhpokcldndlig'
+
+const startChrome = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--bwsi=0',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      `--disable-extensions-except=${extensionPath}`,
+      `--load-extension=${extensionPath}`,
+      '-flag-switches-begin',
+      '--enable-experimental-web-platform-features',
+      '-enable-features=ElementCapture',
+      '--flag-switches-end',
+      '--show-component-extension-options',
+      '--allow-legacy-extension-manifests',
+      '--enable-benchmarking',
+      '--enable-experimental-extension-apis',
+      '--force-dev-mode-highlighting',
+      '--embedded-extension-options',
+      '--whitelisted-extension-id=akniocjldhfgnjjlicnkhpokcldndlig',
+    ],
+    ignoreHTTPSErrors: true,
+    executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+  });
+  // /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --flag-switches-begin --enable-experimental-web-platform-features --enable-features=ElementCapture --flag-switches-end
+
+  //  --flag-switches-begin --enable-experimental-web-platform-features --enable-features=ElementCapture --flag-switches-end
+
+
+  const page = await browser.newPage();
+  await page.goto(`https://github.com`);
+
+
+
+}
+
+startChrome();
--- packages/tabstream/background.js
+++ packages/tabstream/background.js
@@ -6,10 +6,29 @@ console.log('background.js')
 //   });
 // });
 
+let streamObj = null;
+const getPageStream = async () => {
+
+  const id = await chrome.tabCapture.getMediaStreamId();
+  console.log('id', id);
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      mandatory: {
+        chromeMediaSource: 'tab',
+        chromeMediaSourceId: id,
+      },
+    },
+    audio: false,
+  });
+  // const [videoTrack] = stream.getVideoTracks();
+  streamObj = stream;
+}
 
 
 chrome.tabs.onActivated.addListener(async (tab) => {
   console.log('chrome tab.onClicked', tab)
+
+  await getPageStream();
   // chrome.action.setBadgeText({
   //   text: "ON",
   // });
