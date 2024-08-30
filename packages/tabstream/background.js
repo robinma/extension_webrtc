45a355f5cad5de77ffb9e679b4c9576b2cdff5bc
mazj <ahmzj@163.com>
2024-08-30 14:41:40 +0800

    dev

--- packages/tabstream/background.js
+++ packages/tabstream/background.js
@@ -1,3 +1,4 @@
+const startCapture = require('./scripts/displayMedia')
 console.log('background.js')
 // chrome.runtime.onInstalled.addListener(async () => {
 //   console.log('chrome runtime.onInstalled')
@@ -6,20 +7,46 @@ console.log('background.js')
 //   });
 // });
 
+console.log('This is a popup!');
+
+// const id = chrome.tabCapture.getMediaStreamId();
+let streamObj = null;
+const getPageStream = async () => {
+
+  const id = await chrome.tabCapture.getMediaStreamId();
+  console.log('id', id);
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      mandatory: {
+        chromeMediaSource: 'tab',
+        chromeMediaSourceId: id,
+      },
+    },
+    audio: false,
+  });
+  // const [videoTrack] = stream.getVideoTracks();
+  streamObj = stream;
+}
+
+const startStream = async () => {
+  try {
+    // let stream = await getPageStream();
+    console.log('stream', streamObj);
+    const video = document.querySelector('video');
+    video.srcObject = streamObj;
+    video.play();
+  }
+  catch (error) {
+    console.log('error', error);
+  }
+}
+
+
+
 
 
 chrome.tabs.onActivated.addListener(async (tab) => {
   console.log('chrome tab.onClicked', tab)
-  // chrome.action.setBadgeText({
-  //   text: "ON",
-  // });
-  // chrome.action.setBadgeBackgroundColor({
-  //   color: "#ff0000",
-  // });
-  // chrome.tabs.executeScript({
-  //   file: "content.js",
-  // });
-  // chrome.tabs.insertCSS({
-  //   file: "content.css",
-  // });
+  startCapture();
+
 });
---
+++ packages/tabstream/scripts/displayMedia.js
@@ -0,0 +1,27 @@
+const displayMediaOptions = {
+  video: {
+    displaySurface: "browser",
+  },
+  audio: {
+    suppressLocalAudioPlayback: false,
+  },
+  preferCurrentTab: false,
+  selfBrowserSurface: "exclude",
+  systemAudio: "include",
+  surfaceSwitching: "include",
+  monitorTypeSurfaces: "include",
+};
+
+async function startCapture(displayMediaOptions) {
+  let captureStream;
+
+  try {
+    captureStream =
+      await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+  return captureStream;
+}
+
+export default startCapture;
